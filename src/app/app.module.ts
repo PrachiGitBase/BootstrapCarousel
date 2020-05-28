a699f64d5c9f3501abf70c0e488d242b3cea1128
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2020-05-28 14:09:30 +0530

    Bootstrap Carousel first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselModule} from 'ngx-bootstrap/carousel';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CarouselModule.forRoot()
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
